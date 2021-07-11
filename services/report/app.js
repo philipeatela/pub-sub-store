b63256fa2571544dad670ba52be511a8fbe8426c
philipeatela <philipe@videopeel.com>
2021-07-11 13:39:17 -0300

    feat: added report service

--- services/report/app.js
+++ services/report/app.js
@@ -3,28 +3,33 @@ const path = require('path')
 
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
-var report = {}
-async function updateReport(products) {
-    for(let product in products) {
-        if(!product.name) {
-            continue
-        } else if(!report[product.name]) {
-            report[product.name] = 1;
-        } else {
-            report[product.name]++;
+const report = {}
+
+function processMessage(msg) {
+    try {
+        const deliveryData = JSON.parse(msg.content)
+        console.log('Attempting to process report...');
+        if (deliveryData.products && deliveryData.products.length) {
+            deliveryData.products.forEach(({ name, value }) => {
+                const currentProductEntry = report[name];
+                if (currentProductEntry) {
+                    const currentProductValue = currentProductEntry.value;
+                    report[name].value = Number(currentProductValue) + Number(value);
+                    return;
+                }
+                report[name] = { value }
+            })
+            console.log('UPDATED REPORT: ', report);
         }
+    } catch(error) {
+        console.log(`X ERROR TO PROCESS: ${error}`)
     }
-
-}
-
-async function printReport() {
-    for (const [key, value] of Object.entries(report)) {
-        console.log(`${key} = ${value} vendas`);
-      }
 }
 
 async function consume() {
     //TODO: Constuir a comunicação com a fila 
+    console.log(`INSCRITO COM SUCESSO NA FILA: ${process.env.RABBITMQ_QUEUE_NAME}`)
+    await (await RabbitMQService.getInstance()).consume(process.env.RABBITMQ_QUEUE_NAME, (msg) => {processMessage(msg)})
 } 
 
 consume()
